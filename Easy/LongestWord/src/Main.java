33d74144e73edcadc925e4793154c2e846ee34b3
Richard Reader <RJReader@gmail.com>
2014-10-08 23:37:57 +0100

    final version

---
+++ Easy/LongestWord/src/Main.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import static java.lang.System.exit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static java.util.logging.Logger.getLogger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author richard
+ */
+public class Main {
+
+    private static final Logger LOG = getLogger(Main.class.getName());
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            final BufferedReader input = new BufferedReader(new FileReader(args[0]));
+            while (input.ready()) {
+                String longestWord = "";
+                String[] words = input.readLine().split("\\s+");
+                for (String word : words) {
+                    if (word.length() > longestWord.length()) {
+                        longestWord = word;
+                    }
+                }
+                System.out.println(longestWord);
+            }
+        } catch (IOException ex) {
+            getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            exit(1);
+        }
+    }
+
+}
