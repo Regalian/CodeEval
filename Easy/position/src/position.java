2eaf5e9c8e62ec99be0646dd5503b5f59fce6c20
Richard Reader <RJReader@gmail.com>
2014-10-08 23:35:02 +0100

    final version

--- Easy/position/src/position.java
+++ Easy/position/src/position.java
@@ -1,8 +1,13 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import static java.lang.Integer.parseInt;
+import static java.lang.Integer.parseInt;
+import static java.lang.Integer.parseInt;
+import static java.lang.System.exit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import static java.util.logging.Logger.getLogger;
 
 /*
  * To change this template, choose Tools | Templates
@@ -22,16 +27,16 @@ public class position {
             final BufferedReader input = new BufferedReader(new FileReader(args[0]));
             while (input.ready()) {
                 final String[] numbers = input.readLine().split(",");
-                final int number = Integer.parseInt(numbers[0]);
-                final int position1 = Integer.parseInt(numbers[1]);
-                final int position2 = Integer.parseInt(numbers[2]);
+                final int number = parseInt(numbers[0]);
+                final int position1 = parseInt(numbers[1]);
+                final int position2 = parseInt(numbers[2]);
                 final int bit1 = number >> (position1 - 1);
                 final int bit2 = number >> (position2 - 1);
                 System.out.println((bit1 & 1) == (bit2 & 1));
             }
         } catch (IOException ex) {
-            Logger.getLogger(position.class.getName()).log(Level.SEVERE, null, ex);
-            System.exit(1);
+            getLogger(position.class.getName()).log(Level.SEVERE, null, ex);
+            exit(1);
         }
     }
 }
