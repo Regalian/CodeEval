20dbc3af7359e6298ce2c9637156832c0413ab76
Richard Reader <RJReader@gmail.com>
2014-10-08 23:39:14 +0100

    final version

---
+++ Easy/SimpleSorting/src/Main.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import static java.lang.System.exit;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static java.util.logging.Logger.getLogger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author richard
+ */
+public class Main {
+
+    private static final Logger LOG = getLogger(Main.class.getName());
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            final BufferedReader input = new BufferedReader(new FileReader(args[0]));
+            while (input.ready()) {
+                final String[] inputs;
+                inputs = input.readLine().split("\\s+");
+                double [] numbers = new double[inputs.length];
+                for (int i = 0; i <inputs.length; i++) {
+                    numbers[i] = Double.parseDouble(inputs[i]);
+                }
+                Arrays.sort(numbers);
+                for (double d: numbers) {
+                    System.out.printf("%.3f ", d);
+                }
+                System.out.println();
+            }
+        } catch (IOException ex) {
+            getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            exit(1);
+        }
+    }
+
+}
