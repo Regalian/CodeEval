39775b96f43864d07b169dab5bfa93b9b3b2521f
Richard Reader <RJReader@gmail.com>
2014-10-08 23:37:25 +0100

    final version

---
+++ Easy/FindAWriter/src/Main.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import static java.lang.System.exit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static java.util.logging.Logger.getLogger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author richard
+ */
+public class Main {
+
+    private static final Logger LOG = getLogger(Main.class.getName());
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            final BufferedReader input = new BufferedReader(new FileReader(args[0]));
+            while (input.ready()) {
+                final String[] inputs;
+                inputs = input.readLine().split("\\|");
+                String[] indices = inputs[1].trim().split("\\s+");
+                for (String index: indices) {
+                    System.out.print(inputs[0].charAt(Integer.parseInt(index.trim()) - 1));
+                }
+                System.out.println();
+            }
+        } catch (IOException ex) {
+            getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            exit(1);
+        }
+    }
+
+}
